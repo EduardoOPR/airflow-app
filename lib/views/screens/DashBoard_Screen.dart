2b7be945ad3bb6e32f8e793ecb9a79b3cb251cdf
Eduardo de Oliveira <eduardooliveirasalgueiro@hotmail.com>
2023-11-09 09:56:37 -0300

    Painel de ventilação

--- lib/views/screens/DashBoard_Screen.dart
+++ lib/views/screens/DashBoard_Screen.dart
@@ -2,6 +2,7 @@ import 'dart:ffi';
 
 import 'package:airflow/themes/my_themes.dart';
 import 'package:flutter/material.dart';
+import 'package:airflow/views/widgets/RailNavigation.dart';
 
 class DashBoard_Screen extends StatefulWidget {
   const DashBoard_Screen({super.key});
@@ -11,7 +12,8 @@ class DashBoard_Screen extends StatefulWidget {
 }
 
 class _DashBoard_ScreenState extends State<DashBoard_Screen> {
-  int _selectedIndex = 0;
+  final int _selectedIndex = 0;
+  bool isSwitched = true;
 
   @override
   Widget build(BuildContext context) {
@@ -58,134 +60,156 @@ class _DashBoard_ScreenState extends State<DashBoard_Screen> {
           Expanded(
             child: Row(
               children: [
-                NavigationRail(
-                  backgroundColor: Colors.white,
-                  minWidth: 60,
-                  selectedIndex: _selectedIndex,
-                  onDestinationSelected: (int index) {
-                    setState(() {
-                      _selectedIndex = index;
-                    });
-                  },
-                  destinations: [
-                    NavigationRailDestination(
-                      icon: Container(
-                          width: 32,
-                          height: 32,
-                          decoration: BoxDecoration(color: Color(0xFFFAF8F8)),
-                          child: const Icon(
-                            Icons.grid_view,
-                            size: 20,
-                          )),
-                      selectedIcon: Container(
-                          width: 32,
-                          height: 32,
-                          decoration: BoxDecoration(color: Color(0xFFDAF7F1)),
-                          child: const Icon(
-                            Icons.grid_view,
-                            size: 20,
-                          )),
-                      label: Text(""),
+                Theme(
+                    data: ThemeData(
+                      highlightColor: Colors.transparent,
+                      splashFactory: NoSplash.splashFactory,
                     ),
-                    NavigationRailDestination(
-                      icon: Container(
-                          width: 32,
-                          height: 32,
-                          decoration: BoxDecoration(color: Color(0xFFFAF8F8)),
-                          child: const Icon(
-                            Icons.signal_cellular_alt_rounded,
-                            size: 20,
-                          )),
-                      selectedIcon: Container(
-                          width: 32,
-                          height: 32,
-                          decoration: BoxDecoration(color: Color(0xFFDAF7F1)),
-                          child: const Icon(
-                            Icons.signal_cellular_alt_rounded,
-                            size: 20,
-                          )),
-                      label: Text(""),
-                    ),
-                    NavigationRailDestination(
-                      icon: Container(
-                          width: 32,
-                          height: 32,
-                          decoration: BoxDecoration(color: Color(0xFFFAF8F8)),
-                          child: const Icon(
-                            Icons.air,
-                            size: 20,
-                          )),
-                      selectedIcon: Container(
-                          width: 32,
-                          height: 32,
-                          decoration: BoxDecoration(color: Color(0xFFDAF7F1)),
-                          child: const Icon(
-                            Icons.air,
-                            size: 20,
-                          )),
-                      label: Text(""),
-                    ),
-                  ],
-                ),
+                    child: RailNavigation(selectedIndex: _selectedIndex)),
                 Padding(
-                  padding: const EdgeInsets.only(left: 21, top: 40),
+                  padding: const EdgeInsets.only(left: 29),
                   //padding: EdgeInsets.all(0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      //Label painel
-                      Wrap(
-                        crossAxisAlignment: WrapCrossAlignment.center,
-                        spacing: 15,
-                        children: [
-                          Text(
-                            'Painel',
-                            style: MyThemes.inter700(
-                                fontSize: 32, textColor: Colors.black),
-                          ),
-                          const Icon(
-                            Icons.grid_view,
-                            size: 35,
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.65,
-                        child: Text(
-                          'Uma visão geral da performance e dos dispositivos ligados.',
-                          style: MyThemes.inter400(
-                              fontSize: 14, textColor: Colors.black),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 50,
                         ),
-                      ),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.1,
-                      ),
-                      Wrap(
-                        crossAxisAlignment: WrapCrossAlignment.center,
-                        spacing: 10,
-                        children: [
-                          Text(
-                            'Performance',
-                            style: MyThemes.inter700(
-                                fontSize: 24, textColor: Colors.black),
+                        //Label painel
+                        Wrap(
+                          crossAxisAlignment: WrapCrossAlignment.center,
+                          spacing: 15,
+                          children: [
+                            Text(
+                              'Painel',
+                              style: MyThemes.inter700(
+                                  fontSize: 32, textColor: Colors.black),
+                            ),
+                            const Icon(
+                              Icons.grid_view,
+                              size: 35,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.65,
+                          child: Text(
+                            'Uma visão geral da performance e dos dispositivos ligados.',
+                            style: MyThemes.inter400(
+                                fontSize: 14, textColor: Colors.black),
                           ),
-                          const Icon(
-                            Icons.signal_cellular_alt_rounded,
-                            size: 43,
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.07,
+                        ),
+                        Wrap(
+                          crossAxisAlignment: WrapCrossAlignment.center,
+                          spacing: 10,
+                          children: [
+                            Text(
+                              'Performance',
+                              style: MyThemes.inter700(
+                                  fontSize: 24, textColor: Colors.black),
+                            ),
+                            const Icon(
+                              Icons.signal_cellular_alt_rounded,
+                              size: 43,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Image.asset(
+                          'assets/images/graph.png',
+                          height: 320,
+                        ),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        //Label ventilação
+                        Wrap(
+                          crossAxisAlignment: WrapCrossAlignment.center,
+                          spacing: 15,
+                          children: [
+                            Text(
+                              'Ventilação',
+                              style: MyThemes.inter700(
+                                  fontSize: 24, textColor: Colors.black),
+                            ),
+                            const Icon(
+                              Icons.air,
+                              size: 35,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.65,
+                          child: Text(
+                            'Controle seus dispositivos, podendo renomear, ligar e controlar temperatura.',
+                            style: MyThemes.inter400(
+                                fontSize: 14, textColor: Colors.black),
                           ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Image.asset(
-                        'assets/images/graph.png',
-                        height: 320,
-                      )
-                    ],
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.07,
+                        ),
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.4206,
+                          width: MediaQuery.of(context).size.width * 0.6795,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              color: Colors.white),
+                          child: Column(
+                            children: [
+                              Icon(
+                                Icons.ac_unit_sharp,
+                                size: 116,
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    'Ar condicionado 1',
+                                    style: MyThemes.inter700(
+                                        fontSize: 20, textColor: Colors.black),
+                                  ),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.border_color_outlined))
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    'Desligado/Ligado',
+                                    style: MyThemes.inter500(
+                                        fontSize: 16, textColor: Colors.black),
+                                  ),
+                                  Switch(
+                                    value: isSwitched,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        isSwitched = value;
+                                      });
+                                    },
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
---
+++ lib/views/widgets/RailNavigation.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+class RailNavigation extends StatefulWidget {
+  const RailNavigation({super.key, required this.selectedIndex});
+
+  final int selectedIndex;
+
+  @override
+  State<RailNavigation> createState() => _RailNavigationState();
+}
+
+class _RailNavigationState extends State<RailNavigation> {
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _selectedIndex = widget.selectedIndex;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationRail(
+      backgroundColor: Colors.white,
+      minWidth: 60,
+      selectedIndex: _selectedIndex,
+      onDestinationSelected: (int index) {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      destinations: [
+        NavigationRailDestination(
+          icon: Container(
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                  color: Color(0xFFFAF8F8),
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Icon(
+                Icons.grid_view,
+                size: 20,
+              )),
+          selectedIcon: Container(
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                  color: Color(0xFFDAF7F1),
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Icon(
+                Icons.grid_view,
+                size: 20,
+                color: Colors.black,
+              )),
+          label: Text(""),
+        ),
+        NavigationRailDestination(
+          icon: Container(
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                  color: Color(0xFFFAF8F8),
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Icon(
+                Icons.signal_cellular_alt_rounded,
+                size: 20,
+              )),
+          selectedIcon: Container(
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                  color: Color(0xFFDAF7F1),
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Icon(
+                Icons.signal_cellular_alt_rounded,
+                size: 20,
+                color: Colors.black,
+              )),
+          label: Text(""),
+        ),
+        NavigationRailDestination(
+          icon: Container(
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                  color: Color(0xFFFAF8F8),
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Icon(
+                Icons.air,
+                size: 20,
+              )),
+          selectedIcon: Container(
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                  color: Color(0xFFDAF7F1),
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Icon(
+                Icons.air,
+                size: 20,
+                color: Colors.black,
+              )),
+          label: Text(""),
+        ),
+        NavigationRailDestination(
+            padding:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.35),
+            icon: Icon(
+              Icons.abc,
+              color: Colors.white.withOpacity(0),
+            ),
+            indicatorColor: Colors.transparent,
+            disabled: true,
+            label: Text("")),
+        NavigationRailDestination(
+          icon: Container(
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                  color: Color(0xFFFAF8F8),
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Icon(
+                Icons.settings_outlined,
+                size: 20,
+              )),
+          selectedIcon: Container(
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                  color: Color(0xFFDAF7F1),
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Icon(
+                Icons.settings_outlined,
+                size: 20,
+                color: Colors.black,
+              )),
+          label: Text(""),
+        ),
+        NavigationRailDestination(
+          icon: Container(
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                  color: Color(0xFFFAF8F8),
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Icon(
+                Icons.logout,
+                size: 20,
+              )),
+          selectedIcon: Container(
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                  color: Color(0xFFDAF7F1),
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Icon(
+                Icons.logout,
+                size: 20,
+                color: Colors.black,
+              )),
+          label: Text(""),
+        ),
+      ],
+    );
+  }
+}
